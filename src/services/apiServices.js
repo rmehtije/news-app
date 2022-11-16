6c778dc5a16187d450ffe2d58305661762abf602
rmehtije <rmehtije@gmail.com>
2022-11-16 14:42:33 +0200

    homework & .env

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,3 +1,3 @@
 export async function getEverything(data) {
-    return await fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=0338aea9c048417e99441ebd8280e42b');
+    return await fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=' + process.env.REACT_APP_API_KEY);
 }
